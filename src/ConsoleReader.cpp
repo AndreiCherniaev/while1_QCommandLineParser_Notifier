ac38b53372f127216eb0d396b5b3a77ee56dca63
AndreiCherniaev <glscene2010@gmail.com>
2024-01-14 00:52:25 +0900

    add second way
    thanks
    https://stackoverflow.com/questions/6878507/using-qsocketnotifier-to-select-on-a-char-device/7389622#comment71257508_7389622

--- src/ConsoleReader.cpp
+++ src/ConsoleReader.cpp
@@ -1,7 +1,7 @@
 //https://stackoverflow.com/questions/6878507/using-qsocketnotifier-to-select-on-a-char-device/7389622#7389622
 #include "ConsoleReader.h"
 #include <QTextStream>
-
+#include <iostream>
 #include <unistd.h> //Provides STDIN_FILENO
 
 ConsoleReader::ConsoleReader(QObject *parent) :
@@ -13,7 +13,14 @@ ConsoleReader::ConsoleReader(QObject *parent) :
 
 void ConsoleReader::text()
 {
+    //main way
     QTextStream qin(stdin);
     QString line = qin.readLine();
     emit textReceived(line);
+
+    /*//second way
+//https://stackoverflow.com/questions/6878507/using-qsocketnotifier-to-select-on-a-char-device/7389622#comment71257508_7389622
+    std::string line;
+    std::getline(std::cin,line);
+    emit textReceived(QString::fromStdString(line));*/
 }
