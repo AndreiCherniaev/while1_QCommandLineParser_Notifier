7e3b905718e89a3469595987d4aa5f40342bb63b
AndreiCherniaev <glscene2010@gmail.com>
2024-01-14 14:12:21 +0900

    Use Adam's method
    thanks https://stackoverflow.com/questions/6878507/using-qsocketnotifier-to-select-on-a-char-device/7389622#comment71257508_7389622

--- src/ConsoleReader.cpp
+++ src/ConsoleReader.cpp
@@ -13,14 +13,14 @@ ConsoleReader::ConsoleReader(QObject *parent) :
 
 void ConsoleReader::text()
 {
-    //main way
+    /*//second way
     QTextStream qin(stdin);
     QString line = qin.readLine();
-    emit textReceived(line);
+    emit textReceived(line);*/
 
-    /*//second way
+    //main way
 //https://stackoverflow.com/questions/6878507/using-qsocketnotifier-to-select-on-a-char-device/7389622#comment71257508_7389622
     std::string line;
     std::getline(std::cin,line);
-    emit textReceived(QString::fromStdString(line));*/
+    emit textReceived(QString::fromStdString(line));
 }
